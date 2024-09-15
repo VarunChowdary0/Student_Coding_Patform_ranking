c2344c98a0eabb71c3ea235fadfb15b176043171
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2024-09-15 22:02:38 +0530

    mlk

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const cors = require('cors')
+
+const app = express();
+
+cors(app);
+
+app.get("/",(req,res)=>{
+    res.send("Hello");
+})
+
+app.listen(5000,()=>{
+    console.log("Server running..")
+})
