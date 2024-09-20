9f8c28aeb0298203b5c3118dc20566d009c11e47
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2024-09-20 11:35:35 +0530

    we

---
+++ server/Untitled-2.sql
@@ -0,0 +1,58 @@
+INSERT INTO Class_Schedule (Department, Semester, Section, Week_Day, Period1, Period2, Period3, Period4, Period5, Period6)
+VALUES
+-- Section A
+('Electronics and Communication Engineering', 5, 'A', 'Monday', 
+ 'Placement and Training', 'Placement and Training', 'Placement and Training', 'CMC|IARE11084|1309', 'PBL|IARE11084|1212', 'PBL|IARE11084|1212'),
+('Electronics and Communication Engineering', 5, 'A', 'Tuesday', 
+ 'Placement and Training', 'Placement and Training', 'Placement and Training', 'MPMC|IARE10882|1309', 'VI LABORATORY|IARE10683|1212', 'VI LABORATORY|IARE10683|1212'),
+('Electronics and Communication Engineering', 5, 'A', 'Wednesday', 
+ 'Placement and Training', 'Placement and Training', 'Placement and Training', 'CS|IARE10624|1309', 'EMI|IARE10574|1309', 'AWP|IARE10584|1309'),
+('Electronics and Communication Engineering', 5, 'A', 'Thursday', 
+ 'CMC|IARE11084|1309', 'MPMC LABORATORY|IARE10683|1212','MPMC LABORATORY|IARE10683|1212',  'MPMC|IARE10882|1309', 'CS|IARE10624|1309', 'LIBRARY|IARE11084|1309'),
+('Electronics and Communication Engineering', 5, 'A', 'Friday', 
+ 'EMI|IARE10574|1309', 'CS|IARE10624|1309', 'AWP|IARE10584|1309', 'Placement and Training', 'Placement and Training', 'Placement and Training'),
+('Electronics and Communication Engineering', 5, 'A', 'Saturday', 
+ 'CS|IARE10624|1309', 'MPMC|IARE10882|1309', 'AWP|IARE10584|1309', 'EMI|IARE10574|1309', 'MPMC|IARE10882|1309', 'CMC|IARE11084|1309'),
+
+-- Section B
+('Electronics and Communication Engineering', 5, 'B', 'Monday', 
+ 'Placement and Training', 'Placement and Training', 'Placement and Training', 'AWP|IARE10817|1311', 'MPMC|IARE10053|1311', 'EMI|IARE10683|1311'),
+('Electronics and Communication Engineering', 5, 'B', 'Tuesday', 
+ 'Placement and Training', 'Placement and Training', 'Placement and Training', 'CS|IARE10696|1311', 'MPMC|IARE10053|1311', 'CMC|IARE10584|1311'),
+('Electronics and Communication Engineering', 5, 'B', 'Wednesday', 
+ 'Placement and Training', 'Placement and Training', 'Placement and Training', 'CS|IARE10696|1311', 'MPMC LABORATORY|IARE10053|1312B', 'MPMC LABORATORY|IARE10053|1312B'),
+('Electronics and Communication Engineering', 5, 'B', 'Thursday', 
+ 'MPMC|IARE10053|1311', 'AWP|IARE10817|1311', 'CS|IARE10696|1311', 'CMC|IARE10584|1311', 'EMI|IARE10683|1311', 'LIBRARY|IARE10683|1311'),
+('Electronics and Communication Engineering', 5, 'B', 'Friday', 
+ 'CMC|IARE10584|1311', 'VI LABORATORY|IARE10683|1312B','VI LABORATORY|IARE10683|1312B', 'Placement and Training', 'Placement and Training', 'Placement and Training'),
+('Electronics and Communication Engineering', 5, 'B', 'Saturday', 
+ 'PBL|IARE10683|1212','PBL|IARE10683|1212', 'EMI|IARE10683|1311', 'MPMC|IARE10053|1311', 'AWP|IARE10817|1311', 'CS|IARE10696|1311'),
+
+-- Section C
+('Electronics and Communication Engineering', 5, 'C', 'Monday', 
+ 'AWP|IARE10947|1305', 'AWP|IARE10947|1305', 'MPMC|IARE10690|1305', 'WCN|IARE11103|1305', 'EMI|IARE10953|1305', 'LIBRARY|IARE10690|2104'),
+('Electronics and Communication Engineering', 5, 'C', 'Tuesday', 
+ 'PBL|IARE11126|1212','PBL|IARE11126|1212','PBL|IARE11126|1212', 'CS|IARE10795|1305', 'WCN|IARE11103|1305', 'MPMC|IARE10690|1305'),
+('Electronics and Communication Engineering', 5, 'C', 'Wednesday', 
+ 'VI LABORATORY|IARE10865|1212','VI LABORATORY|IARE10865|1212','VI LABORATORY|IARE10865|1212', 'CS|IARE10795|1305', 'MPMC|IARE10690|1305', 'EMI|IARE10953|1305'),
+('Electronics and Communication Engineering', 5, 'C', 'Thursday', 
+ 'EMI|IARE10953|1305', 'AWP|IARE10947|1305', 'MPMC|IARE10690|1305', 'WCN|IARE11103|1305', 'CS|IARE10795|1305', ''),
+('Electronics and Communication Engineering', 5, 'C', 'Friday', 
+ 'WCN|IARE11103|1305', 'EMI|IARE10953|1305', 'EMI|IARE10953|1305', 'MPMC|IARE10690|1305', 'CS|IARE10795|1305', 'AWP|IARE10947|1305'),
+('Electronics and Communication Engineering', 5, 'C', 'Saturday', 
+ 'CS|IARE10795|1305', 'WCN|IARE11103|1305', 'AWP|IARE10947|1305', 'VI LABORATORY|IARE10865|1212', '', ''),
+
+-- Section D
+('Electronics and Communication Engineering', 5, 'D', 'Monday', 
+ 'MPMC LABORATORY|IARE10950|1312B', 'WCN|IARE11103|1408', 'AWP|IARE10913|1408', 'CS|IARE11033|1408', '', ''),
+('Electronics and Communication Engineering', 5, 'D', 'Tuesday', 
+ 'MPMC|IARE10950|1305', 'CS|IARE11033|1305', 'EMI|IARE11103|1305', 'AWP|IARE10913|1408', 'WCN|IARE11103|1408', ''),
+('Electronics and Communication Engineering', 5, 'D', 'Wednesday', 
+ 'CS|IARE11033|1305', 'AWP|IARE10913|1305', 'EMI|IARE11103|1305', 'MPMC|IARE10950|1408', 'WCN|IARE11103|1408', 'LIBRARY|IARE11033|1408'),
+('Electronics and Communication Engineering', 5, 'D', 'Thursday', 
+ 'EMI|IARE11103|1408', 'WCN|IARE11103|1408', 'CS|IARE11033|1408', 'PBL|IARE11126|1212','PBL|IARE11126|1212','PBL|IARE11126|1212'),
+('Electronics and Communication Engineering', 5, 'D', 'Friday', 
+ 'MPMC|IARE10950|1408', 'CS|IARE11033|1408', 'AWP|IARE10913|1408', 'VI LABORATORY|IARE11103|1212','VI LABORATORY|IARE11103|1212','VI LABORATORY|IARE11103|1212'),
+('Electronics and Communication Engineering', 5, 'D', 'Saturday', 
+ 'EMI|IARE11103|1408', 'WCN|IARE11103|1408', 'MPMC|IARE10950|1408', 'AWP|IARE10913|1305', 'EMI|IARE11103|1408', 'SPORTS|IARE11040|1305');
+
