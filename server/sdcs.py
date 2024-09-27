99fbd05cc4d70f94600d3bd12a8e9b465e68b979
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2024-09-27 22:02:54 +0530

    efd

--- server/sdcs.py
+++ server/sdcs.py
@@ -35,3 +35,43 @@
 #     print(n,"is power of 2")
 # else:
 #     print(n,"is Not power of 2")
+
+
+class Solution:
+    def largestInteger(self, num: int) -> int:
+        str_num = str(num)
+        len_f = len(str_num)//2
+        tempp = []
+        for i in str_num:
+            tempp.append(i)
+        print(tempp)
+        e = []
+        o = []
+        for i in tempp:
+            if(int(i)%2==0):
+                e.append(i)
+            else:
+                o.append(i)
+        for i in range(len(tempp)):
+            print(tempp[i])
+            if tempp[i] in e:
+                print(e)
+                mx = max(e)
+                e.remove(mx)
+                idx = tempp.index(mx)
+                tempp[i],tempp[idx] = tempp[idx],tempp[i]
+            if tempp[i] in o:
+                print(o)
+                mx = max(o)
+                o.remove(mx)
+                idx = tempp.index(mx)
+                tempp[i],tempp[idx] = tempp[idx],tempp[i]
+            
+        print(tempp)
+        # print(int("".join(tempp)))
+        return int("".join(tempp))
+
+
+s = Solution()
+s.largestInteger(1234)
+s.largestInteger(65875)
